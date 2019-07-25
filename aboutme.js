e938df0b54df5b45cdf1434c774c7d53e6e2065d
graeme2balint <43181698+graeme2balint@users.noreply.github.com>
2019-07-25 18:31:00 -0400

    Delete aboutme.js

--- aboutme.js
+++
@@ -1,7 +0,0 @@
-var hobbies = document.getElementsByClassName("hob-list");
-
-for (var i = 0; i < hobbies.length; i++) {
-	hobbies[i].addEventListener("click", function() {
-		this.style.color = "red";
-	});
-}
